7c05eedcddc1aa15e4e53716a15965b8b03f3f95
dionysus <dionysus.cho@gmail.com>
2020-10-18 21:09:25 -0700

    updated canvas

--- shortcuts/dl_canvas.js
+++ shortcuts/dl_canvas.js
@@ -1,12 +1,18 @@
-javascript: const el = document.createElement("div");
-el.innerHTML =
-  "<a download" +
-  "Download Video"
-    .link(document.querySelector("audio").getAttribute("src"))
-    .substring(2);
+javascript: audios = document.querySelectorAll("audio");
+el = document.createElement("div");
 el.style.position = "absolute";
 el.style.width = "100%";
 el.style.zIndex = "9999";
 el.style.padding = "10px";
 el.style.backgroundColor = "magenta";
+var i;
+for (i = 0; i < audios.length; i++) {
+  newLink = document.createElement("div");
+  newLink.innerHTML =
+    "<a download" +
+    "Download Link"
+      .link(document.querySelector("audio").getAttribute("src"))
+      .substring(2);
+  el.appendChild(newLink);
+}
 document.body.prepend(el);
